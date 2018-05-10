9436d9284a93b24d7aa39328e723b7145431884b
Dmitry Snegirev <rikkitp@gmail.com>
2018-05-10 09:25:27 +0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,42 @@
+import random
+
+THINGS = [
+  'Thing 1',
+  'Thing 2',
+  'Thing 3'
+]
+
+things = THINGS[:]
+result = []
+
+comparisons = []
+
+while True:
+    choice = None
+    comparison = random.sample(things, 2)
+
+    if (comparison[0], comparison[1]) in comparisons:
+        choice = "1"
+    elif (comparison[1], comparison[0]) in comparisons:
+        choice = "2"
+    else:
+        print("\n\nВыберите:")
+        for i, thing in enumerate(comparison):
+            print("%d. %s" % (i + 1, thing))
+        choice = input("> ")
+
+    if choice == "1":
+        comparisons.append((comparison[0], comparison[1]))
+        things.remove(comparison[1])
+    elif choice == "2":
+        comparisons.append((comparison[1], comparison[0]))
+        things.remove(comparison[0])
+
+    if len(things) < 2:
+        result.append(things[0])
+        things = list(set(THINGS[:]) - set(result))
+        if len(things) < 2:
+            print("\n\nИтог:")
+            for i, thing in enumerate(result + things):
+                print("%d. %s" % (i + 1, thing))
+            exit()
