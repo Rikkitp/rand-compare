78b7b2164f4309f27cd3e38d4a99fdd6f97ba434
Dmitry Snegirev <rikkitp@gmail.com>
2019-12-07 08:16:32 +0300

    Add python types

--- index.py
+++ index.py
@@ -1,25 +1,13 @@
+import pathlib
 import random
 import sys
-import pathlib
+from typing import Optional, List
 
 THINGS = []
 
-if len(sys.argv) == 2:
-    with open(sys.argv[1]) as f:
-        for line in f:
-            THINGS.append(line.strip())
-    path = pathlib.Path(sys.argv[1]).stem
 
-things = THINGS[:]
-result = []
-comparisons = []
-
-def finish():
-    save(True)
-    exit()
-
-def save(all=False):
-    things0 = list(result + things) if all else list(result)
+def save(result: List[str], things: List[str], all_things: bool = False):
+    things0: List[str] = list(result + things) if all_things else list(result)
 
     print("\n\nResult:")
 
@@ -29,30 +17,49 @@ def save(all=False):
                 print(" %d. %s" % (i + 1, thing))
                 f.write("%d. %s\n" % (i + 1, thing))
 
-while True:
-    choice = None
-    comparison = random.sample(things, 2)
 
-    if (comparison[0], comparison[1]) in comparisons:
-        choice = "1"
-    elif (comparison[1], comparison[0]) in comparisons:
-        choice = "2"
-    else:
-        print("\n\nChoose (%d left to #%d):" % (len(things), len(result) + 1))
-        for i, thing in enumerate(comparison):
-            print("%d. %s" % (i + 1, thing))
-        choice = input("> ")
+def main(things) -> None:
+    things = THINGS[:]
+    result = []
+    comparisons = []
 
-    if choice == "1":
-        comparisons.append((comparison[0], comparison[1]))
-        things.remove(comparison[1])
-    elif choice == "2":
-        comparisons.append((comparison[1], comparison[0]))
-        things.remove(comparison[0])
+    while True:
+        choice: Optional[str] = None
+        comparison = random.sample(things, 2)
+        if (comparison[0], comparison[1]) in comparisons:
+            choice = "1"
+        elif (comparison[1], comparison[0]) in comparisons:
+            choice = "2"
+        else:
+            print("\n\nChoose (%d left to #%d):" % (len(things), len(result) + 1))
+            for i, thing in enumerate(comparison):
+                print("%d. %s" % (i + 1, thing))
+            choice = input("> ")
 
-    if len(things) < 2:
-        result.append(things[0])
-        things = list(set(THINGS[:]) - set(result))
-        save()
+        if choice in ("1", "2"):
+            a1i = int(choice) - 1
+            a2i = 1 ^ a1i
+            comparisons.append((comparison[a1i], comparison[a2i]))
+            things.remove(comparison[a2i])
+        elif "-" in choice:
+            ai = int(choice[1]) - 1
+            things.remove(comparison[ai])
+            THINGS.remove(comparison[ai])
+
         if len(things) < 2:
-            finish()
+            result.append(things[0])
+            things = list(set(THINGS[:]) - set(result))
+            save(result, things)
+            if len(things) < 2:
+                save(result, things, True)
+                exit()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            for line in f:
+                THINGS.append(line.strip())
+        path = pathlib.Path(sys.argv[1]).stem
+
+    main(THINGS)
